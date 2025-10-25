bbc8407eda04b194d88815a5c13da08354c4ef5c
DanielMagar <magar.t.daniel@gmail.com>
2025-10-25 21:57:14 +0530

    Church activities

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 import Home from "./pages/Home/Home";
 import AboutUs from "./pages/About/AboutUs";
 import GetInvolved from "./pages/GetInvolved/GetInvolved";
 import Donate from "./pages/Donate/Donate";
 import Layout from "./components/Layout/Layout";
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Layout>
       <Routes>
         <Route path="/" element={<Home />} />
